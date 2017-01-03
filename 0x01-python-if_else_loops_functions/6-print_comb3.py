bd6f9ed5ee3014e0e3231f543ad766811f0235a8
Corbin <corbin.coleman@holbertonschool.com>
2017-01-03 22:27:29 +0000

    Task 6 - Print out the smallest combination of two digits

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 8):
-    for j in range(1, 9):
+for i in range(0, 9):
+    for j in range(1, 10):
         if j > i:
-            print("{:d}{:d}, ".format(i, j), end="")
-print("{:d}".format(89))
+            print("{:d}{:d}".format(i, j), end="")
+            if i != 8:
+                print(", ", end="")
+            else:
+                print("")
