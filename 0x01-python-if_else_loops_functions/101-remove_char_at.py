0f984dfcecee2f9b89b3143bdaebf9f875733d5e
Corbin <corbin.coleman@holbertonschool.com>
2017-01-04 00:02:39 +0000

    Check for None (NULL)

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -2,10 +2,12 @@
 def remove_char_at(str, n):
     pos = 0
     newstr = ""
-    for i in str:
-        if pos == n:
-            pos += 1
-            continue
-        newstr += i
-        pos += 1
-    return (newstr)
+    if str != None:
+        for i in str:
+            if pos == n:
+                pos += 1
+                continue
+                newstr += i
+                pos += 1
+                return (newstr)
+    return (str)
