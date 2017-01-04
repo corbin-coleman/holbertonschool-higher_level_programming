644d4bb97d731f6455702e1a82bac393ec94e05b
Corbin <corbin.coleman@holbertonschool.com>
2017-01-04 00:10:11 +0000

    Do it slightly differently

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    pos = 0
-    newstr = ""
-    for i in str:
-        if pos == n:
-            pos += 1
-            continue
-        newstr += i
-        pos += 1
+    newstr = str
+    if n >= 0:
+        newstr = str[:n] + str[n + 1:]
     return (newstr)
