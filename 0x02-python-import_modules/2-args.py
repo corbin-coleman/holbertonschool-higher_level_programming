7b48b5ff41af2dd7d4ee3f20a10cdc519486948d
Corbin <corbin.coleman@holbertonschool.com>
2017-01-05 18:12:47 +0000

    Don't execute on import

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-import sys
-print("{:d} ".format(len(sys.argv) - 1), end="")
-if len(sys.argv) - 1 == 0:
-    print("argument.")
-else:
-    print("arguments:".format(len(sys.argv) - 1))
-    for i in range(1, len(sys.argv)):
-        print("{:d}: {}".format(i, sys.argv[i]))
+if __name__ == "__main__":
+    import sys
+    print("{:d} ".format(len(sys.argv) - 1), end="")
+    if len(sys.argv) - 1 == 0:
+        print("argument.")
+    else:
+        print("arguments:".format(len(sys.argv) - 1))
+        for i in range(1, len(sys.argv)):
+            print("{:d}: {}".format(i, sys.argv[i]))
