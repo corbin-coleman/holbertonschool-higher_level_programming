e7a433d0a53c38539a32f975e05e2625e5f67a7a
Corbin <corbin.coleman@holbertonschool.com>
2017-01-05 18:56:50 +0000

    Task 6 - Make a calculator

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if sys.argv[2] == '+':
+        print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    elif sys.argv[2] == '-':
+        print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    elif sys.argv[2] == '*':
+        print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    elif sys.argv[2] == '/':
+        print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Only: +, -, /, and * available")
+        sys.exit(1)
+    sys.exit(0)
