510c9b420d6f96d8f061141d75b2427863257023
Corbin <corbin.coleman@holbertonschool.com>
2017-01-05 20:20:29 +0000

    Don't show names starting with __

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for i in range(0, len(dir(hidden_4))):
-        if dir(hidden_4)[i][:1] != '_':
+        if dir(hidden_4)[i][0:2] != '__':
             print(dir(hidden_4)[i])
