64093cf1778cbe134db871308a214325951acaae
Corbin <corbin.coleman@holbertonschool.com>
2017-01-05 18:11:14 +0000

    Task 2 - Print the number of arguments and the arguments themselves

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import sys
+print("{:d} ".format(len(sys.argv) - 1), end="")
+if len(sys.argv) - 1 == 0:
+    print("argument.")
+else:
+    print("arguments:".format(len(sys.argv) - 1))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, sys.argv[i]))
