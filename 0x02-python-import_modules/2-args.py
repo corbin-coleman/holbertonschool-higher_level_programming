e81b4d448b64b6dcf09bc79d8f2f7715ec92649a
Corbin <corbin.coleman@holbertonschool.com>
2017-01-06 03:35:54 +0000

    Works with 1 arg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} ".format(len(sys.argv) - 1), end="")
-    if len(sys.argv) - 1 == 0:
+    count = len(sys.argv) - 1
+    print("{:d} ".format(count), end="")
+    if count == 0:
         print("argument.")
     else:
-        print("arguments:".format(len(sys.argv) - 1))
+        if count == 1:
+            print("argument:".format(count))
+        else:
+            print("arguments:".format(count))
         for i in range(1, len(sys.argv)):
             print("{:d}: {}".format(i, sys.argv[i]))
