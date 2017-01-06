f02dc35c7caba5d6057df0f65c5a364ca00fa514
Corbin <corbin.coleman@holbertonschool.com>
2017-01-06 03:45:47 +0000

    Change error

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     from sys import argv, exit
     if len(argv) - 1 != 3:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     a = int(argv[1])
     b = int(argv[3])
