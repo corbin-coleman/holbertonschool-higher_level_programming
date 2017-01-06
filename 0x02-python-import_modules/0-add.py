f2717e5b93817773f12f3844e0c699e645901da6
Corbin <corbin.coleman@holbertonschool.com>
2017-01-06 00:07:16 +0000

    use a and b

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-from add_0 import add
 if __name__ == "__main__":
-    print("{:d} + {:d} = {:d}".format(1, 2, add(1, 2)))
+    from add_0 import add
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
