17e976be362233422535c670a5f85c54cd0d5611
Corbin <corbin.coleman@holbertonschool.com>
2017-01-10 23:03:58 +0000

    Check for None

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list):
-    my_list.reverse()
-    for ele in my_list:
-        print("{:d}".format(ele))
+    if my_list != None:
+        my_list.reverse()
+        for ele in my_list:
+            print("{:d}".format(ele))
