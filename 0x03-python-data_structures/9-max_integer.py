5ebfed2881094a175be2ddcb67ffb31c73b1a951
Corbin <corbin.coleman@holbertonschool.com>
2017-01-10 23:51:01 +0000

    Task 9 - Find the largest integer in a list

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def max_integer(my_list):
+    if not my_list:
+        return (None)
+    large = 0
+    for i in my_list:
+        if i > large:
+            large = i
+    return (large)
