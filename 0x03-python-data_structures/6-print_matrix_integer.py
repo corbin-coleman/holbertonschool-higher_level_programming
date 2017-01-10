e29f504e3a3e46599a4528d812497e0910de5197
Corbin <corbin.coleman@holbertonschool.com>
2017-01-10 22:19:18 +0000

    Task 6 - Print a matrix of integers

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    index = 0
+    for i in matrix:
+        for j in matrix[index]:
+            print("{:d}".format(j), end="")
+            if j != matrix[index][-1]:
+                print(" ",end="")
+        print("")
+        index += 1
