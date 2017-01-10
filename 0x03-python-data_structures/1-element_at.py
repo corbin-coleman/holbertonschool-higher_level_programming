e30d867f3c3a604d6486d506ae47967cfb3b8856
Corbin <corbin.coleman@holbertonschool.com>
2017-01-10 20:54:58 +0000

    Make it make more sense

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,7 +2,7 @@
 def element_at(my_list, idx):
     my_idx = 0
     for ele in my_list:
-        my_idx += 1
-        if my_idx == idx + 1:
+        if my_idx == idx:
             return (ele)
+        my_idx += 1
     return (None)
