a5249cda243dec213136b4164921a37fc9259ce1
Corbin <corbin.coleman@holbertonschool.com>
2017-01-11 00:10:44 +0000

    Check for empty list or negative index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,6 @@
 def delete_at(my_list=[], idx=0):
     if not my_list or idx < 0:
         return (my_list)
-    del my_list[idx]
+    if idx < len(my_list):
+        del my_list[idx]
     return (my_list)
