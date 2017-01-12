3684684484dda7588f3ced59baf4032ee12e0f1a
Corbin <corbin.coleman@holbertonschool.com>
2017-01-12 22:01:07 +0000

    Pep8

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,5 @@
 def best_score(my_dict):
     if not my_dict:
         return None
-    return "{:s}".format([key for key, v in my_dict.items() if v == max(my_dict[x] for x in my_dict)][0])
+    return "{:s}".format([key for key, v in my_dict.items()
+                          if v == max(my_dict[x] for x in my_dict)][0])
