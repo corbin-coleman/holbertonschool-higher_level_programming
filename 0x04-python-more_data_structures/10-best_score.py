7f287f88077499c1ebf0c867767762c8d6a11f39
Corbin <corbin.coleman@holbertonschool.com>
2017-01-12 22:00:19 +0000

    Task 10 - Find the highest value in a dictionary return the key

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def best_score(my_dict):
+    if not my_dict:
+        return None
+    return "{:s}".format([key for key, v in my_dict.items() if v == max(my_dict[x] for x in my_dict)][0])
