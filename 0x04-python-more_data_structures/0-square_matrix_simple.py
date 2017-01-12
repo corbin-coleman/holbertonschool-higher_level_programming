2ca0552ad2148d3602b4cd8416db734ab8c20d3e
Corbin <corbin.coleman@holbertonschool.com>
2017-01-12 19:34:43 +0000

    Task 0 - Square every item in a matrix and return a new matrix

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def square(x):
+    return x ** 2
+
+def square_matrix_simple(matrix=[]):
+    new_list = []
+    for i in matrix:
+        new_list.append(list(map(square, i)))
+    return new_list
