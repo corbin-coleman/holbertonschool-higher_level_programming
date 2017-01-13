ba6e38b844f5b7a953b29835fa324297b1dcfd17
Corbin <corbin.coleman@holbertonschool.com>
2017-01-13 01:25:55 +0000

    Pep8

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return [list(map(lambda sublist: list(map(lambda val: val ** 2, sublist)), matrix))]
+    return [list(map(lambda sub: list(map(lambda x: x ** 2, sub)), matrix))]
