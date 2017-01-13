51969f524c10dfba226c14e6038da89ede0cf779
Corbin <corbin.coleman@holbertonschool.com>
2017-01-13 20:09:47 +0000

    Task 0 - Print a list up to a given index, don't fail if index is too large

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,6 +6,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             printed += 1
         except IndexError:
-            print("")
             break
+    print("")
     return (printed)
