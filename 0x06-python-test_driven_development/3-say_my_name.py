c114253a4276093ab4270482bc8d45d7138394e2
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 00:21:00 +0000

    Adding files

---
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This is the Name module
+
+This module contains a simple function to print out a first and last name
+"""
+
+def say_my_name(first_name, last_name=""):
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    if type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
