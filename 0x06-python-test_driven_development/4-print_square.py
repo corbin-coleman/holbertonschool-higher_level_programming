d997cff1198ced9411afd215f52e6c16379c556c
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 01:21:14 +0000

    Task 4 - Print a square and the tests

---
+++ 0x06-python-test_driven_development/4-print_square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+"""
+This is the ``Square`` module
+
+It contains a the ``print_square`` function that prints a square of '#'s
+"""
+
+
+def print_square(size):
+    """
+    print out a square of given size
+    """
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    for col in range(size):
+        for row in range(size):
+            print("#", end="")
+        print("")
+    if size == 0:
+        print("")
