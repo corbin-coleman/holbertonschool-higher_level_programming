9e30366fe76d091f0cb76ee5f9b433dbc6e973c6
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 00:24:13 +0000

    Catch exceptions and print them out

--- 0x06-python-test_driven_development/3-say_my_name.py
+++ 0x06-python-test_driven_development/3-say_my_name.py
@@ -14,4 +14,7 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     if type(last_name) != str:
         raise TypeError("last_name must be a string")
-    print("My name is {} {}".format(first_name, last_name))
+    try:
+        print("My name is {} {}".format(first_name, last_name))
+    except Exception as e:
+        print(e)
