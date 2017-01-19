22a3e26f9dc148b30264d61530d91b6376b604e3
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 05:04:17 +0000

    pep8

--- 0x06-python-test_driven_development/5-text_indentation.py
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -17,4 +17,4 @@ def text_indentation(text):
     if not text or not isinstance(text, str) or len(text) < 0:
         raise TypeError("text must be a string")
     new = "".join([let if let not in "?.:" else let + "\n\n" for let in text])
-    print ("\n".join([words.strip() for words in new.split("\n")]), end="")
+    print("\n".join([words.strip() for words in new.split("\n")]), end="")
