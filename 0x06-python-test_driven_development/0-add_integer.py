f28c57e0ff859daf45508a76e2810d17c4c0d59b
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 00:13:35 +0000

    Change int conversion

--- 0x06-python-test_driven_development/0-add_integer.py
+++ 0x06-python-test_driven_development/0-add_integer.py
@@ -14,4 +14,8 @@ def add_integer(a, b):
         raise TypeError("a must be an integer")
     if type(b) != int and type(b) != float:
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
+    if type(a) == float:
+        a = int(a)
+    if type(b) == float:
+        b = int(b)
+    return a + b
