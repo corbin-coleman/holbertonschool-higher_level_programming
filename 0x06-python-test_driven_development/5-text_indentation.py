2202a2c755fb1e14813d18335bc317dc078a8913
Corbin <corbin.coleman@holbertonschool.com>
2017-01-19 02:41:11 +0000

    Task 5 - Print two new lines after finding specific characters and add the tests to check output

---
+++ 0x06-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+"""
+The ``Indentation`` module
+
+This module contains a function to print two new lines after thes characters: '?', '.', ':'
+There should be no extra whitespace at the beginning or the end of the line
+"""
+
+
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    new_text = ""
+    for let in text:
+        i = 0
+        new_text += let
+        string_to_print = ""
+        if let in ['?', '.', ':']:
+            while new_text[i] == ' ':
+                i += 1
+            while i != len(new_text):
+                string_to_print += new_text[i]
+                if new_text[i] in ['?', '.', ':']:
+                    break
+                i += 1
+            print("{:s}\n".format(string_to_print))
+            new_text = ""
