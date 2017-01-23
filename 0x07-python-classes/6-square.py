d2ff7bc42dba1141929ed2f12ed6a7710daec465
Corbin <corbin.coleman@holbertonschool.com>
2017-01-23 23:13:47 +0000

    Task 6 - Print out a square based on a position and size, position prints newlines for y value and spaces for x value

---
+++ 0x07-python-classes/6-square.py
@@ -0,0 +1,45 @@
+class Square:
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return (self.__position)
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(position, tuple):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        for i in position:
+            if not isinstance(i, int):
+                raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        spaces = ' ' * self.__position[0];
+        square_print = '#'
+        if self.__size != 0:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                print("{}{}".format(spaces, square_print * self.__size))
+        else:
+            print("")
