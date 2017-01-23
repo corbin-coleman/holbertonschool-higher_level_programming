8f2681cee1200246a1b6066acd9a97a4204b3f34
Corbin <corbin.coleman@holbertonschool.com>
2017-01-23 22:21:27 +0000

    Task 5 - Create an instance method to print out a square based on size

---
+++ 0x07-python-classes/5-square.py
@@ -0,0 +1,29 @@
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        return (self.__size ** 2)
+
+    @property
+    def size(self):
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def my_print(self):
+        square_print = '#'
+        if self.__size == 0:
+            print("")
+        for i in range(self.__size):
+            print("{}".format(square_print * self.__size))
