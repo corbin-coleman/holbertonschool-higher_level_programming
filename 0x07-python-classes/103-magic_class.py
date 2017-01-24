dbfb8dff03deeabdc53fa31c3fbe54599008e69e
Corbin <corbin.coleman@holbertonschool.com>
2017-01-24 02:33:30 +0000

    Task 10 - Copy some bytecodes

---
+++ 0x07-python-classes/103-magic_class.py
@@ -0,0 +1,12 @@
+class MagicClass:
+    def __init__(self, radius):
+        self.__radius = radius
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        return (self.__radius ** 2 * math.pi)
+
+    def circumference(self):
+        return (2 * math.pi * self.__radius)
