0b84265274b6e41cfdef6c9fd12e66f1f72c4568
corbin-coleman <corbincol@hotmail.com>
2017-01-30 21:15:12 +0000

    Task 9 - print() and str() methods and implement area() method

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+    def area(self):
+        return self.__width * self.__height
