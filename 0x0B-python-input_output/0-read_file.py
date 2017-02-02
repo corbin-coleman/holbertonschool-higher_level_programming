00df552f3424f29eea2990820e27eff2572db99d
corbin-coleman <corbincol@hotmail.com>
2017-02-02 20:08:09 +0000

    Follow the requirements; use with

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def read_file(filename=""):
-    my_file = open(filename, 'r')
-    print(my_file.read(), end="")
-    my_file.close()
+    with open(filename, 'r') as my_file:
+        print(my_file.read(), end="")
