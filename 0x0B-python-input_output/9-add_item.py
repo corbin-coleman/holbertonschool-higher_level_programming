9a44ca44521637fa5e7e3b3350219e952f86c427
corbin-coleman <corbincol@hotmail.com>
2017-02-02 22:34:33 +0000

    Task 9 - Script that adds all arguments passed to a Python list and saves it to a file

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+save_json = __import__('7-save_to_json_file').save_to_json_file
+load_json = __import__('8-load_from_json_file').load_from_json_file
+from sys import argv
+import json
+import os.path
+
+my_file = 'add_item.json'
+if os.path.isfile(my_file):
+    my_list = load_json(my_file)
+else:
+    my_list = []
+count = 0
+for item in argv:
+    if count != 0:
+        my_list.append(item)
+    count += 1
+save_json(my_list, my_file)
