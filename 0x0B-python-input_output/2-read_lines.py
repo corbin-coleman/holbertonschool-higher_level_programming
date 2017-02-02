98284acfe79e3174bdc6aa56d3b716e8d4d205e8
corbin-coleman <corbincol@hotmail.com>
2017-02-02 20:21:37 +0000

    Task 2 - Read a given number of lines from a file

---
+++ 0x0B-python-input_output/2-read_lines.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def read_lines(filename="", nb_lines=0):
+    with open(filename, 'r') as my_file:
+        if nb_lines <= 0:
+            print(my_file.read(), end="")
+        else:
+            for i in range(nb_lines):
+                print(my_file.readline(), end="")
