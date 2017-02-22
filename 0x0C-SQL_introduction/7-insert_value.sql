48fbff3227c480e35ee8ce7946bfc90223ddcbf4
corbin-coleman <corbincol@hotmail.com>
2017-02-22 20:47:02 +0000

    Task 7 - Insert a row into a table in a database on a MySQL server

---
+++ 0x0C-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,3 @@
+-- Insert a new row in a table in a database in a MySQL server
+-- Use first_table table in given database (passed to mysql as an argument)
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton");
