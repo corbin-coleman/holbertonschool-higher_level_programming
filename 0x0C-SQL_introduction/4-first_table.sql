743f3ca8a16f2f0e6bd0b7c0faf14582f01d036d
corbin-coleman <corbincol@hotmail.com>
2017-02-22 19:15:00 +0000

    Task 4 - create a table in a database on a MySQL server

---
+++ 0x0C-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- Create a table in a database on a MySQL server
+-- Database is passed to mysql as an argument, don't create the table if it already exists
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
