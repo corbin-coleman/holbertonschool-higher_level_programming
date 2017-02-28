1cbfc762f27010497c49a5030a5fc61f04ff13f2
corbin-coleman <corbincol@hotmail.com>
2017-02-28 23:30:19 +0000

    One line

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,2 @@
 -- Create a new user on localhost
-CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
-FLUSH PRIVILEGES;
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost IDENTFIED BY 'user_0d_1_pwd';
