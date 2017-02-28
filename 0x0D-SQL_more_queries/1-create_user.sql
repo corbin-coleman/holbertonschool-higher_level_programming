a68bbb7e825a39cc763ed9c9ed10ee8a1cc22d2c
corbin-coleman <corbincol@hotmail.com>
2017-02-28 23:32:17 +0000

    Spelling

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- Create a new user on localhost
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 FLUSH PRIVILEGES;
