fac5f596beab98474a2e272e7d9d72a1585de513
corbin-coleman <corbincol@hotmail.com>
2017-02-28 21:03:57 +0000

    Task 6 - Create a table with a Primary Key column

---
+++ 0x0D-SQL_more_queries/6-states.sql
@@ -0,0 +1,4 @@
+-- Create a database and a table
+-- The table must be in the database created
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
