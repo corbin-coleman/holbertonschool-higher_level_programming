c0d100771a60a4dae1bafae74f95afce0cccda49
corbin-coleman <corbincol@hotmail.com>
2017-02-28 23:25:47 +0000

    Remove a comment

--- 0x0D-SQL_more_queries/1-create_user.sql
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -1,5 +1,4 @@
 -- Create a new user on localhost
--- Give user all permissions
 CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
 FLUSH PRIVILEGES;
