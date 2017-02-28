9f5e63a8b2050a2587bc8f94ee8b572689309297
corbin-coleman <corbincol@hotmail.com>
2017-02-28 20:30:58 +0000

    Task 1 - Create a new user with all privileges

---
+++ 0x0D-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,5 @@
+-- Create a new user on localhost
+-- Give user all permissions
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
+FLUSH PRIVILEGES;
