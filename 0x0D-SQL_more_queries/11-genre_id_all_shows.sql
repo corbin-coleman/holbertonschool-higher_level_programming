4ea02b6e4636dd4e45a046b4ea0f75951beb75cc
corbin-coleman <corbincol@hotmail.com>
2017-02-28 23:12:00 +0000

    Task 11 - Include rows without certain values

---
+++ 0x0D-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,3 @@
+-- Get data from two tables in a database
+-- use JOIN
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id ORDER BY tv_shows.title ASC;
