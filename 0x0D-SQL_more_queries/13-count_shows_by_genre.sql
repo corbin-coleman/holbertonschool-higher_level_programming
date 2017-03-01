e096ca5c274dbfeb30684d12876ab5cc0d5ffea4
corbin-coleman <corbincol@hotmail.com>
2017-03-01 00:09:01 +0000

    Change outpu name

--- 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0D-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,2 +1,2 @@
 -- Count up shows by genre
-SELECT tv_genres.name, COUNT(tv_show_genres.genre_id) AS 'number_shows' FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.`name` ORDER BY number_shows DESC;
+SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.genre_id) AS 'number_shows' FROM tv_genres INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id GROUP BY tv_genres.`name` ORDER BY number_shows DESC;
