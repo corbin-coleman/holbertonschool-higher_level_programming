4f4fe57b471540cac82e3996dd7db6649e902fde
corbin-coleman <corbincol@hotmail.com>
2017-03-01 01:12:17 +0000

    No second select

--- 0x0D-SQL_more_queries/14-my_genres.sql
+++ 0x0D-SQL_more_queries/14-my_genres.sql
@@ -1,2 +1,2 @@
 -- List out all genres of a show
-SELECT tv_genres.name FROM tv_genres JOIN tv_show_genres ON tv_show_genres.show_id = (SELECT tv_shows.id FROM tv_shows WHERE tv_shows.title = "Dexter") WHERE tv_show_genres.genre_id = tv_genres.id ORDER BY tv_genres.name ASC;
+SELECT DISTINCT tv_genres.name FROM tv_show_genres INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id WHERE tv_shows.title = "Dexter" ORDER BY tv_genres.name ASC;
