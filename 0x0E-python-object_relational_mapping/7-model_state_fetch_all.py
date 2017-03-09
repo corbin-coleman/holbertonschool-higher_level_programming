78ccdb277dfbf0690fb16b84d197d2d633a267a3
corbin-coleman <corbincol@hotmail.com>
2017-03-09 02:22:33 +0000

    Fix pep8

--- 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0E-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -6,7 +6,9 @@ import MySQLdb
 
 
 if __name__ == '__main__':
-    database = sqlalchemy.create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    mysql = "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2],
+                                                        argv[3])
+    database = sqlalchemy.create_engine(mysql)
     Base.metadata.create_all(database)
     session_fake = sqlalchemy.orm.sessionmaker(bind=database)
     session = session_fake()
