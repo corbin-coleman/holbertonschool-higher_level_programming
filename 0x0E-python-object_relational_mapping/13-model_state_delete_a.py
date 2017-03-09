e3edf6df131e5e07634c35d7d4ed292a4e0b9930
corbin-coleman <corbincol@hotmail.com>
2017-03-09 03:24:52 +0000

    Task 13 - Delete all states with an a

---
+++ 0x0E-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+import sqlalchemy
+from model_state import State, Base
+import MySQLdb
+
+
+if __name__ == '__main__':
+    mysql = "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2],
+                                                        argv[3])
+    database = sqlalchemy.create_engine(mysql)
+    Base.metadata.create_all(database)
+    session_fake = sqlalchemy.orm.sessionmaker(bind=database)
+    session = session_fake()
+    for element in session.query(State).filter(State.name.like('%a%')).order_by(State.id):
+        session.delete(element)
+    session.commit()
