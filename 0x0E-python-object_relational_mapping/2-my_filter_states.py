71258fe9f958e0763ef2e492bcf899ec52be4641
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:38:32 +0000

    Order ASC by id

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,8 @@ import MySQLdb
 if __name__ == '__main__':
     the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     commands = the_usa.cursor()
-    select_command = "SELECT * FROM states WHERE name = '{:s}'".format(argv[4])
+    select_command = "SELECT * FROM states WHERE name = '{:s}'\
+    ORDER BY id ASC".format(argv[4])
     commands.execute(select_command)
     usa = commands.fetchall()
     for state in usa:
