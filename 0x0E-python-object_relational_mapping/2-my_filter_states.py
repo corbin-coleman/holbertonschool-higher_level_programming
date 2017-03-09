501f1110708731b6756e3a537a7e8bc7e8733935
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:36:21 +0000

    use string formatting

--- 0x0E-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0E-python-object_relational_mapping/2-my_filter_states.py
@@ -5,10 +5,10 @@ import MySQLdb
 if __name__ == '__main__':
     the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     commands = the_usa.cursor()
-    commands.execute("SELECT * FROM states")
+    select_command = "SELECT * FROM states WHERE name = '{:s}'".format(argv[4])
+    commands.execute(select_command)
     usa = commands.fetchall()
     for state in usa:
-        if (state[1] == argv[4]):
-            print(state)
+        print(state)
     commands.close()
     the_usa.close()
