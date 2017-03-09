55175836e35cb26e3012d0d8562b3a9434f68149
corbin-coleman <corbincol@hotmail.com>
2017-03-09 03:05:41 +0000

    Task 11 - Create a new state in database

---
+++ 0x0E-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+from sys import argv
+import sqlalchemy
+from model_state import State, Base
+import MySQLdb
+
+
+if __name__ == '__main__':
+    mysql = "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2],
+                                                        argv[3])
+    database = sqlalchemy.create_engine(mysql)
+    Base.metadata.create_all(database)
+    session_fake = sqlalchemy.orm.sessionmaker(bind=database)
+    session = session_fake()
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    state_added = session.query(State).filter_by(name="Louisiana").first()
+    print("{}".format(state_added.id))
