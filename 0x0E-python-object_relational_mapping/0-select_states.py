4a2243439121666a80cdcb0a5078eb9436b4ac12
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:04:59 +0000

    Protect from running on import, close cursor

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -2,10 +2,12 @@
 from sys import argv
 import MySQLdb
 
-the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-commands = the_usa.cursor()
-commands.execute("SELECT * FROM states")
-usa = commands.fetchall()
-for state in usa:
-    print(state)
-the_usa.close()
+if __name__ == '__main__':
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    commands = the_usa.cursor()
+    commands.execute("SELECT * FROM states")
+    usa = commands.fetchall()
+    for state in usa:
+        print(state)
+    comands.close()
+    the_usa.close()
