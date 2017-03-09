143921d4b8491d50e50800c4cc80363f87767ec5
corbin-coleman <corbincol@hotmail.com>
2017-03-09 02:29:36 +0000

    Grab first state

---
+++ 0x0E-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from sys import argv
+import sqlalchemy
+from model_state import State, Base
+import MySQLdb
+
+
+if __name__ == '__main__':
+    mysql = "mysql+mysqldb://{}:{}@localhost/{}".format(argv[1], argv[2],
+                                                        argv[3])
+    database = sqlalchemy.create_engine(mysql)
+    Base.metadata.create_all(database)
+    session_fake = sqlalchemy.orm.sessionmaker(bind=database)
+    session = session_fake()
+    for element in session.query(State).order_by(State.id):
+        if element.id == 1:
+            print("{}: {}".format(element.id, element.name))
