3656fff43d3804ae8a558ae237525c11512a0877
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:09:05 +0000

    Take out localhost

--- 0x0E-python-object_relational_mapping/0-select_states.py
+++ 0x0E-python-object_relational_mapping/0-select_states.py
@@ -3,7 +3,7 @@ from sys import argv
 import MySQLdb
 
 if __name__ == '__main__':
-    the_usa = MySQLdb.connect(host="localhost", user=argv[1], passwd=argv[2], db=argv[3])
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
     commands = the_usa.cursor()
     commands.execute("SELECT * FROM states ORDER BY id ASC")
     usa = commands.fetchall()
