a20c19ecc747d44a5afd96c226479dfe4b2775a3
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:43:28 +0000

    Order ASC, protect from import

--- 0x0E-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0E-python-object_relational_mapping/4-cities_by_state.py
@@ -2,10 +2,13 @@
 from sys import argv
 import MySQLdb
 
-the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-commands = the_usa.cursor()
-commands.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id")
-usa = commands.fetchall()
-for state in usa:
-    print(state)
-the_usa.close()
+if __name__ == '__main__':
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    commands = the_usa.cursor()
+    commands.execute("SELECT cities.id, cities.name, states.name FROM\
+    cities JOIN states ON cities.state_id = states.id")
+    usa = commands.fetchall()
+    for state in usa:
+        print(state)
+    commands.close()
+    the_usa.close()
