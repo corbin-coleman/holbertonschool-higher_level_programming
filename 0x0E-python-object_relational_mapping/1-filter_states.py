6030fe232f565f592047a826c881942a84570ab0
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:11:39 +0000

    close cursor, order ascending

--- 0x0E-python-object_relational_mapping/1-filter_states.py
+++ 0x0E-python-object_relational_mapping/1-filter_states.py
@@ -2,10 +2,11 @@
 from sys import argv
 import MySQLdb
 
-the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-commands = the_usa.cursor()
-commands.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-usa = commands.fetchall()
-for state in usa:
-    print(state)
-the_usa.close()
+if __name__ == '__main__':
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    commands = the_usa.cursor()
+    commands.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    usa = commands.fetchall()
+    for state in usa:
+        print(state)
+    the_usa.close()
