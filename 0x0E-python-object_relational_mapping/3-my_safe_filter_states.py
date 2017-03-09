f2703b24494b51b4e0fc4b4b0b7374bdd1a7fa6c
corbin-coleman <corbincol@hotmail.com>
2017-03-09 00:41:08 +0000

    Close database, order ASC

--- 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0E-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,11 +2,13 @@
 from sys import argv
 import MySQLdb
 
-the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
-commands = the_usa.cursor()
-commands.execute("SELECT * FROM states")
-usa = commands.fetchall()
-for state in usa:
-    if (state[1] == argv[4]):
-        print(state)
-the_usa.close()
+if __name__ == '__main__':
+    the_usa = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    commands = the_usa.cursor()
+    commands.execute("SELECT * FROM states ORDER BY id ASC")
+    usa = commands.fetchall()
+    for state in usa:
+        if (state[1] == argv[4]):
+            print(state)
+    commands.close()
+    the_usa.close()
