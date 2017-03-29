f38a4022bcb9fed38b5573bf0d38cebdcdb6a9bf
corbin-coleman <corbincol@hotmail.com>
2017-03-29 16:36:08 +0000

    Protect on import

--- 0x10-python-network_1/1-hbtn_header.py
+++ 0x10-python-network_1/1-hbtn_header.py
@@ -3,6 +3,7 @@ import sys
 import urllib.request
 
 
-with urllib.request.urlopen(sys.argv[1]) as response:
-    info = response.info()
-    print(info.get('X-Request-Id'))
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        info = response.info()
+        print(info.get('X-Request-Id'))
