00e64552da5ca7b8dcd504c74ef25fdb6d1bae54
corbin-coleman <corbincol@hotmail.com>
2017-03-29 15:53:22 +0000

    Store response in variable

--- 0x10-python-network_1/0-hbtn_status.py
+++ 0x10-python-network_1/0-hbtn_status.py
@@ -2,7 +2,8 @@
 import urllib.request
 
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    fetched = response.read()
     print("Body response:")
-    print("\t- type:", response.read())
-    print("\t- content:", response.read())
-    print("\t- utf8 content:", response.read().decode('utf-8'))
+    print("\t- type:", type(fetched))
+    print("\t- content:", fetched)
+    print("\t- utf8 content:", fetched.decode('utf-8'))
