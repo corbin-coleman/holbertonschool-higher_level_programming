d654ff0edf2acb5aac369fff8f9ddf3b657372c3
corbin-coleman <corbincol@hotmail.com>
2017-03-29 17:09:50 +0000

    Task 3 - Print the error code

---
+++ 0x10-python-network_1/3-error_code.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+import urllib.error
+import urllib.request
+
+
+if __name__ == '__main__':
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print('Error code:', error.code)
