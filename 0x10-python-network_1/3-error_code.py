a317aab708b78d841313f4c0a218e46fdbbf50e8
corbin-coleman <corbincol@hotmail.com>
2017-03-29 17:14:36 +0000

    Use format string

--- 0x10-python-network_1/3-error_code.py
+++ 0x10-python-network_1/3-error_code.py
@@ -9,4 +9,4 @@ if __name__ == '__main__':
         with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except urllib.error.HTTPError as error:
-        print('Error code:', error.code)
+        print('Error code: {}'.format(error.code))
