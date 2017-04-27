8066921767130cc2cac16f56bdbda2d1c012c32e
Corbin <corbincol@hotmail.com>
2017-04-27 20:01:31 +0000

    Messed up full output

--- 0x11-javascript-warm_up/5-to_integer.js
+++ 0x11-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
 if (parseInt(process.argv[2])) {
-    console.log(parseInt(process.argv[2]));
-}
-else {
-    console.log('Not a number');
+  console.log('My number:', parseInt(process.argv[2]));
+} else {
+  console.log('Not a number');
 }
