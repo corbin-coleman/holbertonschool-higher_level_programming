2a001683680ce7dc89e84507d954a876f741e25e
Corbin <corbincol@hotmail.com>
2017-04-28 21:08:44 +0000

    Semistandard fix

--- 0x12-javascript-web_scraping/6-square.js
+++ 0x12-javascript-web_scraping/6-square.js
@@ -5,17 +5,17 @@ const Rectangle = require('./4-rectangle').Rectangle;
 module.exports = {
   Square: function (size) {
     Rectangle.call(this, size, size);
-    this.charPrint = function(c) {
-	if (c === undefined) {
-	  c = 'X';
-        }
-	let row = '';
-	for (let i = 0; i < size; i++) {
-          row = row.concat(c);
-        }
-        for (let i = 0; i < size; i++) {
-          console.log(row);
-        }
-    }
+    this.charPrint = function (c) {
+      if (c === undefined) {
+        c = 'X';
+      }
+      let row = '';
+      for (let i = 0; i < size; i++) {
+        row = row.concat(c);
+      }
+      for (let i = 0; i < size; i++) {
+        console.log(row);
+      }
+    };
   }
 };
