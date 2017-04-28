b9daf5766545a0516e8d2bc58b099ee7c8720cbe
Corbin <corbincol@hotmail.com>
2017-04-28 20:21:57 +0000

    Task 4 - Rotate rectangle, double rectangles size

---
+++ 0x12-javascript-web_scraping/4-rectangle.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+module.exports = {
+  Rectangle: function (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+    this.print = function() {
+      row = '';
+      for (let i = 0; i < this.width; i++) {
+        row = row.concat('X');
+      }
+      for (let i = 0; i < this.height; i++) {
+	console.log(row);
+      }
+    }
+    this.rotate = function() {
+      tmp = this.height;
+      this.height = this.width;
+      this.width = tmp;
+    }
+    this.double = function() {
+      this.height *= 2;
+      this.width *= 2;
+    }
+  }
+};
