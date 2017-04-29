c12df45f03056d3261ff51b9f2fd5f7f48151116
Corbin <corbincol@hotmail.com>
2017-04-29 05:39:33 +0000

    Task 12 - Write a file from a URL request

---
+++ 0x12-javascript-web_scraping/12-request_store.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(url, function (error, response, body) {
+    fs.writeFile(file, body, 'utf8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+});
