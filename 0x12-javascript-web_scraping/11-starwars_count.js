327ba0c28debc3efd71c3de8ec2388d48427144b
Corbin <corbincol@hotmail.com>
2017-04-29 05:21:34 +0000

    Task 11 - Count the films Wedge Antilles is in in the Star Wars series

---
+++ 0x12-javascript-web_scraping/11-starwars_count.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+const request = require('request');
+const baseURL = process.argv[2];
+
+request(baseURL, function (error, response, body) {
+  let films = JSON.parse(body)['results'];
+  let count = 0;
+  for (let i = 0; i < films.length; i++) {
+    let filmDict = films[i];
+    let filmCharacters = filmDict['characters'];
+    for (let j = 0; j < filmCharacters.length; j++) {
+      if (filmCharacters[j].match(/18/)) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
+});
