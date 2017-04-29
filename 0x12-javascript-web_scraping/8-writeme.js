baf5c511feb272478bd6d60d6725ca60382c8f61
Corbin <corbincol@hotmail.com>
2017-04-29 05:23:22 +0000

    Make sure it is UTF-8 encoded

--- 0x12-javascript-web_scraping/8-writeme.js
+++ 0x12-javascript-web_scraping/8-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const file = process.argv[2];
 const text = process.argv[3];
 
-fs.writeFile(file, text, function (error) {
+fs.writeFile(file, text, 'utf8', function (error) {
   if (error) {
     return console.log(error);
   }
