1efac3585d3cdfe06ec89e1dd9cbcc5477fc853e
corbin-coleman <corbincol@hotmail.com>
2017-05-02 20:56:44 +0000

    Task 18 - Toggle a tags class between two classes

---
+++ 0x13-javascript-web_jquery/18-script.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+$('div#toggle_header').click(function () {
+  if ($('header').hasClass('red')) {
+    $('header').removeClass('red').addClass('green');
+  } else {
+    $('header').removeClass('green').addClass('red');
+  }
+});
